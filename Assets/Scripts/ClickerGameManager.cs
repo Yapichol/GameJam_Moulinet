c9d46aeeb27a34d2a0cbdbfb48bde47245826410
Yapichol <yapichol.karlcleuziou@gmail.com>
2023-10-08 01:45:49 +0200

    Click bug fixed

--- Assets/Scripts/ClickerGameManager.cs
+++ Assets/Scripts/ClickerGameManager.cs
@@ -11,7 +11,7 @@ public class ClickerGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        StartClickSession();
     }
 
     // Update is called once per frame
